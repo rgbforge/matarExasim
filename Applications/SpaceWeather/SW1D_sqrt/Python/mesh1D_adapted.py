7d1eee6c09e817345c860f53244f4f2fd115f17f
opalissan <opal.issan@gmail.com>
2022-10-13 10:07:04 -0700

    [OI] mesh file needs comments. These paramaters can be used for UQ.

--- Applications/SpaceWeather/SW1D_sqrt/Python/mesh1D_adapted.py
+++ Applications/SpaceWeather/SW1D_sqrt/Python/mesh1D_adapted.py
@@ -1,35 +1,45 @@
-from numpy import *
+"""Module to create an adapted mesh.
+# TODO: comments. """
+import numpy as np
 from scipy.optimize import fsolve
 
 
 def mesh1D_adapted(r1, r2, nx):
+    """1D adapted mesh
+
+    :param r1: ??
+    :param r2: ??
+    :param nx: ??
+    :return: p, t
+    """
     dlay = 1
     dwall = 1e-2
 
     # Calculate mesh ratio
     c = 1 - dlay / dwall
-
-    def function(x):
-        return scalingfun(x, nx, c)
-
-    rat = fsolve(function, 1.1)
+    # TODO: what is this 1.1?
+    rat = fsolve(lambda x_val: scalingfun(x_val, nx, c), 1.1)
     rat = rat[0]
 
-    xv = zeros(nx + 1)
+    xv = np.zeros(nx + 1)
     xv[1] = dwall
     for i in range(1, nx):
         xv[i + 1] = xv[i] + dwall * rat ** i
 
-    # if abs(xv[-1]-dlay)>1e-8:
-    # error("Something wrong with the input parameters")
-
-    p = array([xv * (r2 - r1) + r1])
-    t = array([[k, k + 1] for k in range(0, nx)])
+    p = np.array([xv * (r2 - r1) + r1])
+    t = np.array([[k, k + 1] for k in range(0, nx)])
     t = t.transpose()
     return p, t
 
 
 def scalingfun(x, n, c):
+    """??
+
+    :param x: ??
+    :param n: ??
+    :param c: ??
+    :return: ??
+    """
     F = c
 
     for i in range(1, n):
